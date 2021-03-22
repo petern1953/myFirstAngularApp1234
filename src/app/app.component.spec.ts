5978eaafba83bb580103d9712b9f3ab847da2123
petern1953 <petern1953@gmail.com>
2021-03-22 17:42:51 +0100

    readme json src from 123d

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myFirstAngularApp1234'`, () => {
+  it(`should have as title 'myFirstAngularApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myFirstAngularApp1234');
+    expect(app.title).toEqual('myFirstAngularApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('myFirstAngularApp1234 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('myFirstAngularApp app is running!');
   });
 });
